1a88ea49498227c5474560fdcf9ef3c6ff197c4e
Manish Yadav <yadav7.manish7@gmail.com>
2022-10-31 12:17:11 -0500

    created bankTransaction fucntion

--- src/day2/FunctionPractice.java
+++ src/day2/FunctionPractice.java
@@ -3,7 +3,6 @@ package day2;
 public class FunctionPractice {
 
     // int x = 10; variable
-    int transType;
 
     public static void main(String[] args) {
 
@@ -11,10 +10,16 @@ public class FunctionPractice {
         float rate = 6.32f;   //f to denote float type
         int timeInYears = 5;
 
-        calculateInterest(principal, rate, timeInYears);  //
+        calculateInterest(principal, rate, timeInYears);  // calling a method
 
         double calculatedInterestRate = calculateInterest(rate, principal, timeInYears);  // storing returned value into a variable
         System.out.println(calculatedInterestRate);
+
+        deposit(calculatedInterestRate);
+
+        bankTransaction(calculatedInterestRate,1);  // to deposit interest
+        bankTransaction(500,1);  // to deposit
+        bankTransaction(400, 2); // to withdraw
 
     }
 
@@ -25,6 +30,23 @@ public class FunctionPractice {
 
     static double calculateInterest(float r, long p, int t) {
         return (r * p * t) / 100;
+    }
+
+    static void deposit(double interest){
+        // code to deposit interest rate in your balance
+    }
+
+    static void withdraw(double loan){
+        // withdraws loan amount from your account balance
+    }
+    // transType 1 = deposit , 2 = withdraw
+    static void bankTransaction(double amount , int transType){
+
+        if (transType == 1){
+            deposit(amount);
+        }else {
+            withdraw(amount);
+        }
     }
 
 }
