6f9c4486346c715c43a26d2eb09e48434e7dfdef
Manish Yadav <yadav7.manish7@gmail.com>
2022-11-14 14:33:25 -0600

    new employee class created

--- src/day6/Employee.java
+++ src/day6/Employee.java
@@ -1,9 +1,46 @@
 package day6;
 
+import java.time.LocalDate;
+
 public class Employee {
 
-    private String name;
-    private int age;
+    private String empName;
+    private int empAge;
     private String empId;
+    private float empSalary;
+    private LocalDate dateOfHire;
 
+    public Employee(String empName, String empId, float empSalary) {
+        this.empName = empName;
+        this.empId = empId;
+        this.empSalary = empSalary;
+    }
+
+    public Employee(String empName, int empAge, String empId, float empSalary, LocalDate dateOfHire) {
+        this.empName = empName;
+        this.empAge = empAge;
+        this.empId = empId;
+        this.empSalary = empSalary;
+        this.dateOfHire = dateOfHire;
+    }
+
+    public String getEmpName() {
+        return empName;
+    }
+
+    public int getEmpAge() {
+        return empAge;
+    }
+
+    public String getEmpId() {
+        return empId;
+    }
+
+    public float getEmpSalary() {
+        return empSalary;
+    }
+
+    public LocalDate getDateOfHire() {
+        return dateOfHire;
+    }
 }
