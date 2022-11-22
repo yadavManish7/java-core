83fcab8d141447541e481a6eec6f42b8c700a18d
Manish Yadav <yadav7.manish7@gmail.com>
2022-11-21 21:49:58 -0600

    switch case demo

--- src/day8/Student.java
+++ src/day8/Student.java
@@ -24,7 +24,6 @@ public class Student {
 
         int total = 500;
         float sum = math+science+history+social+java;
-<<<<<<< HEAD
         System.out.println(String.format("Total marks obtained %.2f",sum));
         float percent = (sum/total)*100;
         System.out.println(String.format("Percentage %.2f", percent) + " %");
@@ -40,12 +39,9 @@ public class Student {
         }else {
             System.out.println("you have just passed");
         }
-=======
-        System.out.println("Total marks obtained = " + sum);
-        float percent = (sum/total)*100;
-        System.out.println("Percentage = " + percent + "%");
+
 
         scanner.close();
->>>>>>> 8f0f1c985c9519d97f4651b4a2e4de2ce161473b
+
     }
 }
--- src/day9/SwitchDemo.java
+++ src/day9/SwitchDemo.java
@@ -3,5 +3,56 @@ package day9;
 public class SwitchDemo {
     public static void main(String[] args) {
 
+        String day = "SUNDAY";  //switch can check only one variable . Not multiple cases
+        boolean isWeekend=false;
+        switch (day){
+            case "SUNDAY" :
+                isWeekend=true;
+                break;
+            case "MONDAY":
+            case "TUESDAY":
+            case "WEDNESDAY":
+            case "THURSDAY":
+            case "FRIDAY":
+                isWeekend=false;
+                break;
+            case "SATURDAY":
+                isWeekend = true;
+            default:
+                System.out.println("Invalid day");
+        }
+        System.out.println(isWeekend);
+
+        System.out.println("*****************************************");
+
+        int dayOfWeek=3;
+
+        switch (dayOfWeek){    //switch is faster than if-else
+            case 1:
+                System.out.println("Sunday");
+                break;
+            case 2:
+                System.out.println("Monday");
+                break;
+            case 3:
+                System.out.println("Tuesday");
+                break;
+            case 4:
+                System.out.println("Wednesday");
+                break;
+            case 5:
+                System.out.println("Thursday");
+                break;
+            case 6:
+                System.out.println("Friday");
+                break;
+            case 7:
+                System.out.println("Saturday");
+                break;
+            default:
+                System.out.println("Invalid day");
+
+
+        }
     }
 }
